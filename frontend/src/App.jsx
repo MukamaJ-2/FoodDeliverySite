85ce0ee9507c27ef5c48cffb8145e49f872ddb2c
MukamaJ-2 <145841039+MukamaJ-2@users.noreply.github.com>
2025-01-18 14:10:44 +0300

    d

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import NavBar from './components/NavBar/NavBar'
 import {Routes, Route } from 'react-router-dom'
 import Home from './pages/Home/Home'
@@ -6,10 +6,20 @@ import Cart from './pages/Cart/Cart'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
 import Footer from './components/Footer/Footer'
 import LoginPopup from './components/LoginPopup/LoginPopup'
-
+import axios from "axios";
 function App() {
 
   const [showLogin, setShowLogin] = useState(false);
+
+const fetchAPI= async () =>{
+  const response = await axios.get("http://localhost:8080/api");
+  console.log(response.data.fruits);
+};
+
+
+useEffect(() =>{
+  fetchAPI();
+}, []);
 
   return (
     <>
